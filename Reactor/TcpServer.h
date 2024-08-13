1e10aca6b75642c664fb550d7867a0a6f58acd2a
johnwick020213 <ganbo020213@gmail.com>
2024-08-13 17:01:27 +0800

    头文件编写

--- Reactor/TcpServer.h
+++ Reactor/TcpServer.h
@@ -1,9 +1,26 @@
-#ifndef HEADER_FILENAME_H
-#define HEADER_FILENAME_H
+#ifndef __TCPSERVER_H__
+#define __TCPSERVER_H__
 
-#include <iostream>
-#include <string>
+#include "Acceptor.h"
+#include "EventLoop.h"
 
+class TcpServer
+{
+public:
+    TcpServer(const string &ip,unsigned short port);
+    ~TcpServer();
+
+    void start();
+    void stop();
+
+    void setAllCallback(TcpConnectionCallback &&cb1
+                        ,TcpConnectionCallback &&cb2
+                        ,TcpConnectionCallback &&cb3);
+
+private:
+    Acceptor _acceptor;
+    EventLoop _loop;
+};
    
 
 #endif 

b954df81ac119ccae6d6386c1edc6c97cc70a07f
johnwick020213 <ganbo020213@gmail.com>
2024-08-20 23:15:22 +0800

    关键词推荐

--- httpserver/include/KeyRecommander.h
+++ httpserver/include/KeyRecommander.h
@@ -1,20 +1,27 @@
 #ifndef __KEYRECOMMANDER_H__
 #define __KEYRECOMMANDER_H__
-
+#include<wfrest/json.hpp>
 #include <string>
-
+#include<queue>
+#include<set>
 using namespace std;
+
+using Json=nlohmann::json;
 
 class KeyRecommander
 {
 public:
     KeyRecommander(const string word);
-    
+       
+    Json get_result();
 private:
+    void execut();
     void queryIndexTable();
-    
+    void statistic(set<int>& iset);
+    int distance(const string & rhs);//计算最小编辑距离。
 
-    priority_queue<string result> _resultQue;
+
+    priority_queue<string> _resultQue;
     string _queryWord;
     
 };
--- httpserver/src/KeyRecommander.cpp
+++ httpserver/src/KeyRecommander.cpp
@@ -1,2 +1,27 @@
 #include "../include/KeyRecommander.h"
 
+ KeyRecommander::KeyRecommander(const string word) {
+    execut();
+}
+
+Json KeyRecommander::get_result() {
+    Json tmp;
+
+    for(int i=0;i<10;i++)
+    {
+        tmp.push_back(_resultQue.pop());
+    }
+}
+
+void KeyRecommander::execut() {
+
+}
+
+void KeyRecommander::queryIndexTable() {
+
+}
+
+void KeyRecommander::statistic(set<int>& iset) {
+
+}
+
